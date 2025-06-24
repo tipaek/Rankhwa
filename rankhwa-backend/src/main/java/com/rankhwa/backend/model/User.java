a8e28c3d4ad417dca22c45273ea622b55fd1a793
Timothy Paek <tax2farm@gmail.com>
2025-06-24 16:32:27 -0400

    Remove unnecessary AuthProvider import

--- rankhwa-backend/src/main/java/com/rankhwa/backend/model/User.java
+++ rankhwa-backend/src/main/java/com/rankhwa/backend/model/User.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.security.AuthProvider;
 import java.time.Instant;
 
 @Entity
