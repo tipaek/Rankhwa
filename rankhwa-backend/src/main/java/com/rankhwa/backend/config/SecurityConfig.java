49c75d953ec4c84d989e29a47e39ba51cc2ea10a
Timothy Paek <tax2farm@gmail.com>
2025-06-24 16:29:17 -0400

    Added user registration/login with JWT. User db in Neon. DB ping protected endpoint temporarily.

--- rankhwa-backend/src/main/java/com/rankhwa/backend/config/SecurityConfig.java
+++ rankhwa-backend/src/main/java/com/rankhwa/backend/config/SecurityConfig.java
@@ -1,24 +1,34 @@
 package com.rankhwa.backend.config;
 
+import com.rankhwa.backend.security.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired JwtAuthFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/health/sanity-check").permitAll() //temporarily allowing health checkup; remove this later
-                        .requestMatchers("/db-health/ping").permitAll()
+                        .requestMatchers(
+                                        "/auth/**",
+                                        "/health/**"
+                                        //,"/db-health/**" making endpoint protected
+                                ).permitAll()
                         .anyRequest().authenticated()// protecting other endpoints
                 )
-                .httpBasic();
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
