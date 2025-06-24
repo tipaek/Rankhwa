bdf26cbcfe569891a94cf0c1bb68b5bd2bccd465
Timothy Paek <tax2farm@gmail.com>
2025-06-24 13:57:57 -0400

    Added health checkup health/sanity_check endpoint. Also SecurityConfig allowing unauthorized sanity_check access while keeping others

---
+++ rankhwa-backend/src/main/java/com/rankhwa/backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.rankhwa.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/health/sanity_check").permitAll() //temporarily allowing health checkup; remove this later
+                        .anyRequest().authenticated()// protecting other endpoints
+                )
+                .httpBasic();
+        return http.build();
+    }
+}
